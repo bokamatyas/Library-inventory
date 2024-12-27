4d66f17949bff90c8df1b7262e68c53726b6a636
Mátyás Bóka <72026133+bokamatyas@users.noreply.github.com>
2024-12-27 17:45:36 +0100

    Adding data modal and associated changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { BookModel } from './models/book-model';
 import { DataService } from './services/data.service';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { BookDataComponent } from "./components/book-data/book-data.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CardListComponent],
+  imports: [CardListComponent, BookDataComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -24,5 +25,15 @@ export class AppComponent {
       next: (_data: BookModel[]) => { this.books = _data },
       error: (_err) => console.log(_err)
     });
+  }
+
+  newBook(){
+
+  }
+
+  modifyBook(_inBook: BookModel) {
+    this.bookData = _inBook;
+    console.log(this.bookData);
+    
   }
 }
